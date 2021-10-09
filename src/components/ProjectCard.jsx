f979bf3df675804bf2235a96318ad79cc97eb075
Norewal <63234816+Norewal@users.noreply.github.com>
2021-10-09 09:06:23 +0200

    added ProShop project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,94 +1,111 @@
 import React, { useEffect } from 'react';
-import Avatar from "./ProjectAvatar";
-import Detail from "./ProjectDetails";
+import Avatar from './ProjectAvatar';
+import Detail from './ProjectDetails';
 import './ProjectCard.css';
 
-import Aos from "aos";
-import "aos/dist/aos.css";
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 /*import { Button, CardActions } from "react-mdl"; */
 
-
-
-
 function Card(props) {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
 
-    useEffect(() => {
-        Aos.init({ duration: 2000 });
-    }, []);
+  return (
+    <div>
+      <div
+        className='row card'
+        data-aos='fade-up'
+        style={{
+          flexDirection: props.id % 2 === 0 ? 'row-reverse' : 'row',
+        }}
+      >
+        <div className='card-left'>
+          <div className='card-box'>
+            <div
+              className='card-image'
+              onClick={() => {
+                window.open(props.link1);
+              }}
+              style={{ cursor: 'pointer' }}
+            >
+              <Avatar img={props.img} alt={props.title} />
+              {/*<Detail detailInfo={props.id} /> */}
+            </div>
+          </div>
+        </div>
+        <div className='card-right'>
+          <div className='content-box'>
+            <div className='title'>
+              <Detail detailInfo={props.title} />
+            </div>
+            <div className='description'>
+              <Detail
+                detailInfo={props.description}
+                style={{ fontSize: '18px' }}
+              />
+            </div>
+            <div className='sub-title'>
+              <Detail detailInfo={props.subTitle} />
+            </div>
+            <div className='sub-description'>
+              <Detail detailInfo={props.subDescription} />
+            </div>
+            <hr></hr>
 
-    return (
-        <div>
-            <div className="row card" data-aos="fade-up" style={{
-                flexDirection: props.id % 2 === 0 ? "row-reverse" : "row"
-                    }}>
-                <div className="card-left">
-                    <div className="card-box">
-                        <div className="card-image" onClick={() => {window.open(props.link1)}} style={{cursor: "pointer"}}>
-                            <Avatar img={props.img} />
-                            {/*<Detail detailInfo={props.id} /> */}
-                        </div>
-                    </div>
-                </div>
-                <div className="card-right" >
-                    <div className="content-box">
-                        <div className="title">
-                            <Detail detailInfo={props.title} />
-                        </div>
-                        <div className="description">
-                            <Detail detailInfo={props.description} style={{fontSize:"18px"}} />
-                        </div>
-                        <div className="sub-title">
-                            <Detail detailInfo={props.subTitle} />
-                        </div>
-                        <div className="sub-description">
-                            <Detail detailInfo={props.subDescription} />
-                        </div>
-                        <hr></hr>
-                        
-                        
-                        {/*<CardActions border className="buttons">
+            {/*<CardActions border className="buttons">
                             <Button type="button" onClick={() => {window.open(props.link1)}} style={{color: "#fff", backgroundColor: "#83ABA3", border: '1px solid #83ABA3', padding: "5px 5px 15px", marginRight: "5px", marginBottom: "5px"}}><Detail detailInfo={props.buttonLabel} /></Button>
                             <div style={{display: props.buttonLabel2 === "" ? "none" : "inline"}}>
                                 <Button  onClick={() => {window.open(props.link2)}} style={{color: "#83ABA3",  border: '1px solid #83ABA3', padding: "5px 5px 15px", marginBottom: "5px"}}><Detail detailInfo={props.buttonLabel2} /></Button>
                             </div>
                         </CardActions> */}
-                        
-                            <button 
-                                className="btn-green" 
-                                type="button" 
-                                onClick={() => {window.open(props.link1)}}
-                                style={{display: props.buttonLabel === "" ? "none" : "inline"}}
-                            >
-                                <Detail detailInfo={props.buttonLabel} />
-                            </button>
-                            
-                            <div style={{display: props.buttonLabel2 === "" ? "none" : "inline"}}>
-                                <button 
-                                    className="btn-white" 
-                                    type="button" 
-                                    onClick={() => {window.open(props.link2)}}
-                                >
-                                    <Detail detailInfo={props.buttonLabel2} />
-                                </button>
-                            </div>
 
-                            <div style={{display: props.buttonLabel3 === "" ? "none" : "inline"}}>
-                                <button 
-                                    className="btn-yellow" 
-                                    type="button" 
-                                    onClick={() => {window.open(props.link3)}}
-                                    disabled="false"
-                                >
-                                    <Detail detailInfo={props.buttonLabel3} />
-                                </button>
-                            </div>
-                        
-                    </div>
-                </div>
+            <button
+              className='btn-green'
+              type='button'
+              onClick={() => {
+                window.open(props.link1);
+              }}
+              style={{ display: props.buttonLabel === '' ? 'none' : 'inline' }}
+            >
+              <Detail detailInfo={props.buttonLabel} />
+            </button>
+
+            <div
+              style={{ display: props.buttonLabel2 === '' ? 'none' : 'inline' }}
+            >
+              <button
+                className='btn-white'
+                type='button'
+                onClick={() => {
+                  window.open(props.link2);
+                }}
+              >
+                <Detail detailInfo={props.buttonLabel2} />
+              </button>
             </div>
+
+            <div
+              style={{ display: props.buttonLabel3 === '' ? 'none' : 'inline' }}
+            >
+              <button
+                className='btn-yellow'
+                type='button'
+                onClick={() => {
+                  window.open(props.link3);
+                }}
+                disabled='false'
+              >
+                <Detail detailInfo={props.buttonLabel3} />
+              </button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function Detail(props) {
-    return <p className="info">{props.detailInfo}</p>;
+  return <p className='info'>{props.detailInfo}</p>;
 }
 
 export default Detail;
